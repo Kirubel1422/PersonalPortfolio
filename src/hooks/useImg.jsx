02a4e1d388d7e1b75fdd6526da1a0a412c3bddc5
Kirubel <kirubel739@gmail.com>
2024-07-26 00:45:45 +0400

    style(useImg.jsx) gave highlight color to the skeleton

--- src/hooks/useImg.jsx
+++ src/hooks/useImg.jsx
@@ -5,7 +5,14 @@ import Skeleton from "react-loading-skeleton";
 export const useImg = () => {
   return ({ src, alt, style, circle, height, width }) => (
     <Img
-      loader={<Skeleton circle={circle} height={height} width={width} />}
+      loader={
+        <Skeleton
+          highlightColor="#767676"
+          circle={circle}
+          height={height}
+          width={width}
+        />
+      }
       src={src}
       alt={alt}
       className={twMerge(style)}
