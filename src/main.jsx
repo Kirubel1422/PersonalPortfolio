d3b256cf36faf2b7e2742424c199218150345a23
Kirubel <kirubel739@gmail.com>
2024-07-26 01:15:38 +0400

    feat (main.jsx) animated-cursor to be on screen-size higher than 768px in width

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
@@ -7,28 +7,48 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import AnimatedCursor from "react-animated-cursor";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <AnimatedCursor
-        innerSize={20}
-        outerSize={26}
-        color="199, 0, 57"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          "select",
-          "button",
-        ]}
-      />
-      <ToastContainer />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const Main = () => {
+  const [isMob, setIsMob] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsMob(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+        {!isMob && (
+          <AnimatedCursor
+            innerSize={20}
+            outerSize={26}
+            color="199, 0, 57"
+            outerAlpha={0.2}
+            innerScale={0.7}
+            outerScale={5}
+            clickables={[
+              "a",
+              'input[type="text"]',
+              'input[type="email"]',
+              'input[type="number"]',
+              'input[type="submit"]',
+              "select",
+              "button",
+            ]}
+          />
+        )}
+        <ToastContainer />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
