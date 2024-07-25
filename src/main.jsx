28c8a92e1d1947bec99e3ed426ac2472b79dfcfb
Kirubel <kirubel739@gmail.com>
2024-07-26 00:32:46 +0400

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import Cursor from "react-cursor-follow";
 import App from "./App.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import AnimatedCursor from "react-animated-cursor";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Cursor
-        className="z-[100]"
-        hollow
-        duration={0}
-        color={"#ECECEC"}
-        size={45}
+      <AnimatedCursor
+        innerSize={20}
+        outerSize={26}
+        color="199, 0, 57"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          "select",
+          "button",
+        ]}
       />
       <ToastContainer />
     </BrowserRouter>
